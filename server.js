54c4e81a139664ae73202bd9f6562973f6a41150
Joy Agbo <joyagbo49@gmail.com>
2023-11-03 01:07:59 +0100

    Database set up

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 require('dotenv').config
 const app = express()
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ extended: false }));
+//app.use(cookieParser());
+
+//Database connection
+const dbURI = process.env.MONGO_URI;
+mongoose.connect(dbURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("database connected"))
+    .catch((err) => console.log(err));
+
 
 const port = 3000;
 app.listen(port, () => {
